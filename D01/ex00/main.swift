240fac27e3f35cf7ba8f6b545009a91db66650bc
Simon SICARD <ssicard@e2r9p1.42.fr>
2017-04-04 12:44:45 +0200

    sending ex00

---
+++ D01/ex00/main.swift
@@ -0,0 +1,14 @@
+#!/usr/bin/env swift
+print("----- Test ex00 -----\n")
+
+for f in Color.allColors {
+    print(f.rawValue)
+}
+
+print("")
+
+for t in Value.allValues {
+    print(t.rawValue)
+}
+
+print("\n---------------------\n")
